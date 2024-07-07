6ee4597d87b0f7e19ab23406fab32e668e23d804
Neander Fontes <neanderffontes@gmail.com>
2024-07-08 00:35:30 +0100

    feature/create-class-model

--- library/src/main/java/com/library/library/model/LibraryModel.java
+++ library/src/main/java/com/library/library/model/LibraryModel.java
@@ -1,4 +1,65 @@
 package com.library.library.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "library")
 public class LibraryModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "isbn", nullable = false)
+    private String ISBN;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LibraryModel that = (LibraryModel) o;
+        return id.equals(that.id) && ISBN.equals(that.ISBN) && name.equals(that.name) && author.equals(that.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ISBN, name, author);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
