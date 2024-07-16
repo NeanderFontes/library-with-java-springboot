2d6ed7ecb7a4661739adbe3e9088c5e3e128af4e
Neander Fontes <neanderffontes@gmail.com>
2024-07-16 09:50:45 +0100

    Create Design Patter mapper DTO

--- library/src/main/java/com/library/library/mapper/DozerMapper.java
+++ library/src/main/java/com/library/library/mapper/DozerMapper.java
@@ -1,4 +1,23 @@
 package com.library.library.mapper;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DozerMapper {
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <LibraryModel, LibraryDTO> LibraryDTO parseObject(LibraryModel origin, Class<LibraryDTO> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <LibraryModel, LibraryDTO> List<LibraryDTO> parseListObject(List<LibraryModel> origin, Class<LibraryDTO> libraryDTOClass) {
+        List<LibraryDTO> destinationObject = new ArrayList<>();
+        for (LibraryModel objectModel : origin) {
+            destinationObject.add(mapper.map(objectModel, libraryDTOClass));
+        }
+        return destinationObject;
+    }
 }
